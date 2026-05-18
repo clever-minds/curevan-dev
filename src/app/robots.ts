cf98785750fad26a2365e00cd14dbf04ba28ea78
ujjawal <ujjawal.infonex@gmail.com>
2026-05-18 16:40:02 +0530

    facebook post issue

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,21 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{
-      userAgent: '*',
-      allow: '/',
-    }],
+    rules: [
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Facebot',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://www.curevan.com/sitemap.xml',
   }
 }
